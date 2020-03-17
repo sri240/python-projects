d85e0931d9f0bfd8ade2c172907b150465c687e6
art3m1s <anusrisaraf20@gmail.com>
2020-03-16 20:37:04 -0700

    initial commit

---
+++ roses.py
@@ -0,0 +1,78 @@
+from math import sin 
+from math import cos 
+from math import radians 
+  
+import pygame 
+  
+# The width and height of our program 
+(width, height) = (800, 600)
+
+n = 0
+  
+# Setup the window and init the screen surface 
+screen = pygame.display.set_mode((width, height)) 
+  
+pygame.display.set_caption('rose curves :)') 
+  
+# background
+screen.fill((150, 150, 250))
+
+# rose with n petals  of radius of size
+def drawRhodoneaCurve(n, size):
+    points =[] 
+    for i in range(0, 361): 
+        # equation of a rhodonea curve 
+        r = size * sin(radians(n * i)) 
+  
+        # cartesian conversion
+        x = r * cos(radians(i)) 
+        y = r * sin(radians(i)) 
+  
+        list.append(points, (width / 2 + x, height / 2 + y))
+    # draws a set of line segments connected by set of vertices points 
+    pygame.draw.lines(screen, (255, 255, 255), False, points, 2)
+
+
+# maurer rose with value n and d, size of size
+def drawMaurerRose(n, d, size): 
+    points =[] 
+    for i in range(0, 361): 
+        # equation of a maurer rose 
+        k = i * d 
+        r = size * sin(radians(n * k)) 
+  
+        # cartesian conversion
+        x = r * cos(radians(k)) 
+        y = r * sin(radians(k)) 
+  
+        list.append(points, (width / 2 + x, height / 2 + y)) 
+  
+    pygame.draw.lines(screen, (210, 255, 255), False, points, 1) 
+
+def redraw():
+    pygame.display.update()
+    screen.fill((150, 150, 250))
+    drawRhodoneaCurve(n,200)
+
+
+  
+#drawMaurerRose(6, 73, 200) 
+
+
+
+
+  
+# flip the drawn canvas with the newly created canvas (double-buffering) 
+pygame.display.flip() 
+
+run = True
+while True :  
+    # Poll the events in the event queue 
+    for event in pygame.event.get() :
+        # if the user closed the window  
+        if event.type == pygame.QUIT :  
+            pygame.quit()
+            quit()
+        n = n + 0.01
+        redraw()
+        #pygame.display.update()
